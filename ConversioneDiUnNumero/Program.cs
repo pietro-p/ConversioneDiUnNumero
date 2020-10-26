5e6b769450c335a4b2af053583d5f577204eca3f
_ Trentini _ <pietrotrentini5@gmail.com>
2020-10-26 23:11:22 +0100

    Add project files.

---
+++ ConversioneDiUnNumero/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection.Emit;
+
+namespace ConversioneDiUnNumero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int abase, dabase;
+            double numero;
+            Console.Write("Inserisci la base da cui convertire: ");
+            dabase = int.Parse(Console.ReadLine());
+            Console.Write("Inserisci la base in cui convertire: ");
+            abase = int.Parse(Console.ReadLine());
+            Console.Write("inserisci il numero da convertire: ");
+            numero = double.Parse(Console.ReadLine());
+            string risultato = Convert.ToString(Convert.ToInt32(numero, dabase), abase);
+            Console.WriteLine($"il numero convertito è {risultato}");
+        }
+    }
+}
+
